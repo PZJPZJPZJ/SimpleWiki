bb342eda3b93ddbfee1358c441e04a4ed1e63471
PZJ <68857304+PZJPZJPZJ@users.noreply.github.com>
2025-04-16 23:31:25 +0800

    Commit

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,15 +10,6 @@ export default defineUserConfig({
   description: 'Tech Made Simple',
   base: '/SimpleWiki/',
   dest: 'docs/',
-  head: [
-    ['script', {}, `<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "r50baewab4");
-</script>`]
-  ],
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
     navbar: [
