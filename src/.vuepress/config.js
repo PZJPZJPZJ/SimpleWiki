b59f89751c6ec82f5600f4647ad1aad5fbf8568a
PZJ <68857304+PZJPZJPZJ@users.noreply.github.com>
2025-04-26 01:10:05 +0800

    Commit

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,6 +3,14 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { catalogPlugin } from '@vuepress/plugin-catalog'
 
+const clarityScript = `
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "r9rbwus6gb");
+`
+
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'Simple Wiki',
@@ -10,7 +18,7 @@ export default defineUserConfig({
   base: '/SimpleWiki/',
   dest: 'docs/',
   head: [
-    ['script', { src: '/SimpleWiki/clarity.js' }]
+    ['script', { type: 'text/javascript' }, clarityScript]
   ],
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
